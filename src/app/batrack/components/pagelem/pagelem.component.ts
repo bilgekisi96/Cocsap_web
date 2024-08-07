e969f49ab64658afcf77ce84623a537ace6438dd
yavuzselimozkok <yavuzselimozkok@gmail.com>
2024-08-07 15:27:33 +0300

    adjust for fixed maintenance

--- src/app/batrack/components/pagelem/pagelem.component.ts
+++ src/app/batrack/components/pagelem/pagelem.component.ts
@@ -13,4 +13,21 @@ export class PagelemComponent {
   logfunc(){
     this.router.navigate(['/login'])
     }
+  onButtonClickinsta(){
+    window.open('https://www.instagram.com/softwaresolitions', '_blank');
+  }
+  onButtonClickwhatsapp(){
+    window.open('https://chat.whatsapp.com/LQ37MAS0Uto93S75XjPjXe', '_blank');
+
+  }
+  onButtonClicktwetter(){
+
+  }
+  onButtonClickfacebook(){
+
+  }
+
+
+
+
 }
